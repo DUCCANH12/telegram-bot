40704b40c77dc7b26d7a16dde47346bea14fc03b
Canh <105976095+DUCCANH12@users.noreply.github.com>
2025-11-17 13:32:00 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,33 +1,31 @@
+import requests
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-import requests
 
-API_URL = "https://docs.google.com/spreadsheets/d/13j0yTTymeTpuyZCwg46KxtCKKVpgUAjsBr1NbkjWCbE/edit?gid=0#gid=0"  # THAY LINK WEB APP
+TOKEN = "8223648375:AAFcMzVJXhNGpLqTnRS4D3aRKbPQHZkeNls"
+API_URL = "https://script.google.com/macros/s/AKfycbxiC2TGpuduBUHOcyPxnGo61Oicg_eoJjoGnqAPoI73soR-WXhOh6YzatcCMwFkfzlg/exec"   # Dán link ở BƯỚC 1 vào đây
 
 async def add(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text = " ".join(context.args)
-    if not text:
-        await update.message.reply_text("Nhập: /add nội_dung")
+    if not context.args:
+        await update.message.reply_text("Dùng: /add nội_dung")
         return
-
-    r = requests.get(API_URL, params={"action": "add", "value": text})
+    value = " ".join(context.args)
+    r = requests.get(API_URL, params={"action": "add", "value": value})
     await update.message.reply_text(r.text)
 
 async def get(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not context.args:
-        await update.message.reply_text("Nhập: /get số_dòng")
+        await update.message.reply_text("Dùng: /get số_dòng")
         return
-
     row = context.args[0]
     r = requests.get(API_URL, params={"action": "get", "row": row})
-    await update.message.reply_text(f"Dòng {row} cột D: {r.text}")
+    await update.message.reply_text(r.text)
 
 def main():
-    app = ApplicationBuilder().token("8223648375:AAFcMzVJXhNGpLqTnRS4D3aRKbPQHZkeNls").build()
+    app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("add", add))
     app.add_handler(CommandHandler("get", get))
     app.run_polling()
 
 if __name__ == "__main__":
     main()
-
